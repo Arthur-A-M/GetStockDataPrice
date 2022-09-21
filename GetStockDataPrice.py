f25ea3fa76149c0c3d09c840a5383b82d7673513
Arthur <71463136+ta191@users.noreply.github.com>
2022-09-21 09:15:22 -0300

    Adicionada opção de escolher ação e removidos bibliotecas não usadas

--- GetStockDataPrice.py
+++ GetStockDataPrice.py
@@ -1,16 +1,15 @@
-import pandas as pd
 import yfinance as yf
-import datetime
 from datetime import date, timedelta
-today = date.today()
 
+today = date.today()
+stock = str(input('Qual ação da NASDAQ deseja verificar? ').strip().upper())
 d1 = today.strftime("%Y-%m-%d")
 end_date = d1
 d2 = date.today() - timedelta(days=360)
 d2 = d2.strftime("%Y-%m-%d")
 start_date = d2
 
-data = yf.download('AAPL', 
+data = yf.download(stock, 
                       start=start_date, 
                       end=end_date, 
                       progress=False)
